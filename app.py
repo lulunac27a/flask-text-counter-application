3de45a7c569f4391f29cdaee0b680fa4d8e0dbbf
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-30 15:42:04 -0600

    Add type annotations

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route("/")  # index page
-def homepage():  # home page
+def homepage() -> str:  # home page
     return render_template(
         "index.html", characters=0, words=0, lines=0, text_content=""
     )  # return index page template
@@ -12,19 +12,19 @@ def homepage():  # home page
 
 @app.template_filter("comma_separator")  # comma separator filter
 def comma_separator_filter(
-    number,
-):  # get number with comma separator as thousands separator
+    number: int | float,
+) -> str:  # get number with comma separator as thousands separator
     # print number with comma separator as thousands separator
     return f"{number:,}"
 
 
 @app.route("/calculate_text_counter", methods=["POST"])
-def calculate_text_counter():  # calculate text counter
+def calculate_text_counter() -> str:  # calculate text counter
     # text content from text box area
-    text_content = request.form["text_content"]
-    characters = len(text_content)  # number of characters
-    words = len(text_content.split())  # number of words
-    lines = len(text_content.split("\n"))  # number of lines
+    text_content: str = request.form["text_content"]
+    characters: int = len(text_content)  # number of characters
+    words: int = len(text_content.split())  # number of words
+    lines: int = len(text_content.split("\n"))  # number of lines
     return render_template(
         "index.html",
         characters=characters,
