fb7e6302737f53612f3a6d714dbc817a18689af1
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-30 15:37:08 -0600

    Add comments in Python

--- app.py
+++ app.py
@@ -3,26 +3,32 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def homepage():
-    return render_template("index.html", characters=0, words=0, lines=0)
+@app.route("/")  # index page
+def homepage():  # home page
+    return render_template(
+        "index.html", characters=0, words=0, lines=0
+    )  # return index page template
 
 
-@app.template_filter("comma_separator")
-def comma_separator_filter(number):
+@app.template_filter("comma_separator")  # comma separator filter
+def comma_separator_filter(
+    number,
+):  # get number with comma separator as thousands separator
+    # print number with comma separator as thousands separator
     return f"{number:,}"
 
 
 @app.route("/calculate_text_counter", methods=["POST"])
-def calculate_text_counter():
+def calculate_text_counter():  # calculate text counter
+    # text content from text box area
     text_content = request.form["text_content"]
-    characters = len(text_content)
-    words = len(text_content.split())
-    lines = len(text_content.split("\n"))
+    characters = len(text_content)  # number of characters
+    words = len(text_content.split())  # number of words
+    lines = len(text_content.split("\n"))  # number of lines
     return render_template(
         "index.html",
         characters=characters,
         words=words,
         lines=lines,
         text_content=text_content,
-    )
+    )  # return template with updated character, word and line count
